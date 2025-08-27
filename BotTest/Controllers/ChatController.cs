a9777ccb647bdab9d861681d2e642d4d199f9dad
isnuraina <nurane.ismayilzade19@gmail.com>
2025-08-27 17:03:09 +0400

    GetHistory is added to project

--- BotTest/Controllers/ChatController.cs
+++ BotTest/Controllers/ChatController.cs
@@ -1,4 +1,5 @@
-﻿using BotTest.Models;
+﻿using BotTest.Data;
+using BotTest.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,13 @@ namespace BotTest.Controllers
     [ApiController]
     public class ChatController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public ChatController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpPost("send")]
         public ActionResult<ChatResponse> SendMessage([FromBody] ChatRequest request)
         {
@@ -28,7 +36,19 @@ namespace BotTest.Controllers
             {
                 reply = "Uzr isteyirem,sizi tam basa dusmedim!";
             }
+            var history = new ChatHistory
+            {
+                UserMessage = request.Message,
+                BotReply = reply
+            };
+            _context.ChatHistories.Add(history);
+            _context.SaveChanges();
             return Ok(new ChatResponse { Reply = reply });
+        }
+        [HttpGet("history")]
+        public ActionResult<IEnumerable<ChatHistory>> GetHistory()
+        {
+            return Ok(_context.ChatHistories.OrderByDescending(x=>x.CreatedAt).ToList());
         }
     }
 }
